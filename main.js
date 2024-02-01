02c4fa718fda63a99c0f1f19d085c0c211ea1f59
mohammadsofan <mohammadsof72@gmail.com>
2024-02-01 23:38:12 +0200

    v0

---
+++ main.js
@@ -0,0 +1,36 @@
+
+let count=0;
+const addBtn=document.querySelector(".box form input:last-child");
+addBtn.onclick=addTask;
+
+document.querySelector(".box form").addEventListener('submit',function(e){
+    e.preventDefault();
+}
+);
+
+function addTask(){
+    const tasksDiv =document.querySelector('.tasks');
+    const inputTask=document.querySelector(".box form input:first-child").value;
+
+    const content=tasksDiv.innerHTML;
+    const newTask=`
+        <div class='task'>
+            <input type='checkbox' onclick='taskFinished(this)' data-id=${count} >
+            <p data-id=${count}>${inputTask}</p>
+        </div>
+    `;
+    tasksDiv.innerHTML=content + newTask;
+    count++;
+}
+
+function taskFinished(e){
+    if(e.checked){
+    document.querySelector(`.task p[data-id='${e.dataset.id}']`).style.textDecoration='line-through';
+    e.setAttribute('checked','');
+    }
+    else{
+        document.querySelector(`.task p[data-id='${e.dataset.id}']`).style.textDecoration='none';
+        e.removeAttribute('checked')
+    }
+
+}
